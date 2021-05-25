8bd59ca11c243876962e9ded91b6c8ba450a56f3
Benedikt Reinartz <filmor@gmail.com>
2021-05-25 23:04:22 +0200

    Adjust mix files and explicitly specify deps

---
+++ apps/jupyter/mix.exs
@@ -0,0 +1,30 @@
+defmodule IErl.Jupyter.MixProject do
+  use Mix.Project
+  import Mix.Rebar
+
+  def project do
+    args = get_app_src()
+    config = load_config(".")
+
+    [
+      app: :jupyter,
+      version: :erlang.list_to_binary(args[:vsn]),
+      elixir: "~> 1.4",
+      deps: deps(config),
+      erlc_options: config[:erl_opts],
+
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+    ]
+  end
+
+  defp get_app_src do
+    {:ok,
+      [{:application, :jupyter, args}]
+    } = :file.consult "src/jupyter.app.src"
+
+    args
+  end
+end
